cae9cb957bfa0cd1c6999c218b4aa605f2977b8c
ruslan1996r <dshmc27@gmail.com>
2020-06-23 17:02:01 +0300

    some fix22

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -28,34 +28,3 @@ function Planets() {
 }
 
 export default Planets
-
-
-
-// import React, { useContext } from 'react'
-
-// import Planet from "./Planet"
-// import planetContext from "../planetContext/planetContext"
-// import PlanetInfo from './PlanetInfo'
-
-// function Planets() {
-//     const { planets, selectPlanet, selectedPlanet } = useContext(planetContext)
-//     return (
-//         <div className="planets">
-//             {planets.length ? planets.map(planet => {
-//                 return (
-//                     <Planet
-//                         key={planet.id}
-//                         planet={planet}
-//                         selectPlanet={selectPlanet}
-//                     />
-//                 )
-//             }) : <h2>Nothing found</h2>}
-//             {selectedPlanet && <PlanetInfo
-//                 selectedPlanet={selectedPlanet}
-//                 selectPlanet={selectPlanet}
-//             />}
-//         </div>
-//     )
-// }
-
-// export default Planets
